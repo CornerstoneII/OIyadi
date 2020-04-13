36ce657d6e0a6a5108b8515f9ee7a428aef341e8
Iyadi Ol'seun <42003510+CornerstoneII@users.noreply.github.com>
2020-04-13 15:24:13 +0100

    First-Commit

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,40 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+  const income = data.region.avgDailyIncomeInUSD;
+  const population = data.region.avgDailyIncomePopulatin;
+  const hospitalBeds = data.totalHospitalsBeds * 0.35;
+  if (data.periodType === 'weeks') {
+    data.timeToElapse *= 7;
+  } else if (data.periodType === 'months') {
+    data.timeToElapse *= 30;
+  }
+  const days = data.timeToElapse;
+  const factor = Math.trunc(days / 3);
+  const impact = {
+    currentlyInfected: data.reportedCases * 10,
+    get infectionsByRequestedTime() {
+      return this.currentlyInfected * (2 ** factor);
+    },
+    get severCasesByRequestedTime() {
+      return this.infectionsByRequestedTime * 0.15;
+    },
+    get hospitalBedsByRequestedTime() {
+      return Math.ceil(hospitalBeds - this.severCasesByRequestedTime);
+    },
+    get casesForICUByRequestedTime() {
+      return this.infectionsByRequestedTime * 0.05;
+    },
+    get casesForVentilatorsByRequestedTime() {
+      return this.infectionsByRequestedTime * 0.05;
+    },
+    get dollarsInFlight() {
+      return this.infectionsByRequestedTime * 0.05;
+    }
+  };
+  return {
+    data,
+    impact,
+    severImpact
+  };
+};
 
 export default covid19ImpactEstimator;
